8afb77a03d3e8cdb0fa0b9ff4fe6e3b9a8287667
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-20 19:58:12 +0700

    New Branch, Penambahan Activity func

---
+++ app/Repository/ActivityRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace PalaganTeam\MuhKansai\Repository;
+
+use PalaganTeam\MuhKansai\Domain\Activity;
+use PDO;
+class ActivityRepository{
+    private PDO $connection;
+
+    public function __construct(PDO $connection){
+        $this->connection = $connection;
+    }
+
+    /**
+     * Save Activity to DB
+     * 
+     * Menyimpan data Activity ke dalam DB
+     * @return Activity
+     */
+    public function save(Activity $activity): Activity{
+        $stmt = $this->connection->prepare('INSERT INTO activity(nama_activity, tanggal_pembuatan, detail_activity, history_activity) VALUES(?, ?, ?, ?)');
+        $stmt->execute([$activity->namaActivity, $activity->tanggalPembuatan, $activity->detailActivity, $activity->historyActivity]);
+
+        return $activity;
+    }
+
+    /**
+     * Find Activity by ID
+     * 
+     * Mencari data activity berdasarkan ID
+     * @return Activity
+     */
+    public function findById(int $idActivity): ?Activity{
+        $stmt = $this->connection->prepare('SELECT * FROM activity WHERE id = ?');
+        $stmt->execute([$idActivity]);
+
+        try{
+            if($row = $stmt->fetch()){
+                $activity = new Activity;
+                $activity->idActivity = $row['id'];
+                $activity->namaActivity = $row['nama_activity'];
+                $activity->tanggalPembuatan = $row['tanggal_pembuatan'];
+                $activity->detailActivity = $row['detail_activity'];
+                $activity->historyActivity = $row['history_activity'];
+
+                return $activity;
+            }else{
+                return null;
+            }
+        } finally {
+            $stmt->closeCursor();
+        }
+    }
+
+    /**
+     * Get All Activity Data
+     * 
+     * Ambil Semua activity yang ada di dalam database
+     */
+    public function getAllData(): array{
+        $stmt = $this->connection->prepare('SELECT * FROM activity');
+        $stmt->execute();
+
+        try{
+            $row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            foreach($row as $data){
+                $array[] = $data;
+            }
+
+            return $array;
+        } finally{
+            $stmt->closeCursor();
+        }
+    }
+    
+}
