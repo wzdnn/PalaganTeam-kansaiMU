f0a92be8654f53487e2d39966b25cd9e718a1385
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-23 20:49:23 +0700

    Penambahan News Path

--- app/App/DotEnv.php
+++ app/App/DotEnv.php
@@ -4,10 +4,25 @@ namespace PalaganTeam\MuhKansai\App;
 class DotEnv{
     /**
      * Get default timezone
+     * 
+     * Default timezone ialah tokyo time 
      */
     public static function getTimezoneArea(?string $targetTimezone = 'Asia/Tokyo'){
         date_default_timezone_set($targetTimezone);
 
         return date_default_timezone_get();
+    }
+
+    /**
+     * News Path Thumbnail
+     * 
+     * Mengambil path folder untuk news, diperlukan __DIR__ diawal pemanggilan fungsi ini
+     * @return '/../../public/images/news/'
+     */
+    public static function getNewsPath(){
+        // news path
+        $path = '/../../public/images/news/';
+
+        return $path;
     }
 }
