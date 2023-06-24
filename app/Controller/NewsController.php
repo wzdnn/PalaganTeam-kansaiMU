28b3fb1dd88e3aeefa27f49dff43c5ad644ea0a4
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-24 12:41:09 +0700

    penambahan news controller (struktur)

---
+++ app/Controller/NewsController.php
@@ -0,0 +1,52 @@
+<?php
+namespace PalaganTeam\MuhKansai\Controller;
+class NewsController{
+    /**
+     * GET News List Page
+     */
+    public function index(){
+
+    }
+
+    /**
+     * GET News Detail Page
+     */
+    public function news(int $idNews){
+
+    }
+
+    /**
+     * GET News Create Page
+     */
+    public function newsCreate(){
+
+    }
+
+    /**
+     * POST News Create Page
+     */
+    public function postNewsCreate(){
+
+    }
+
+    /**
+     * GET News Update Page
+     */
+    public function newsUpdate(int $idNews){
+
+    }
+
+    /**
+     * POST News Update Page
+     */
+    public function postNewsUpdate(int $idNews){
+
+    }
+
+    /**
+     * POST News Delete
+     */
+    public function postNewsDelete(int $idNews){
+        
+    }
+}
