63065703bbddb316d95038b5cb8f8edc50fc63de
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-25 00:48:16 +0700

    Penambahan midleware pada path

--- public/index.php
+++ public/index.php
@@ -3,15 +3,20 @@
 use PalaganTeam\MuhKansai\App\Router;
 use PalaganTeam\MuhKansai\Controller\HomeController;
 use PalaganTeam\MuhKansai\Controller\UserController;
+use PalaganTeam\MuhKansai\Middleware\AlreadyLogin;
+use PalaganTeam\MuhKansai\Middleware\MustLoginMiddleware;
 
 require_once(__DIR__ . "./../vendor/autoload.php");
 
 // Home Page Index(default)
-Router::add('GET', '/', HomeController::class, 'index');
+Router::add('GET', '/', HomeController::class, 'index', [MustLoginMiddleware::class]);
 
-Router::add('GET', '/login', UserController::class, 'login');
-Router::add('POST', '/login', UserController::class, 'postLogin');
+Router::add('GET', '/login', UserController::class, 'login', [AlreadyLogin::class]);
+Router::add('POST', '/login', UserController::class, 'postLogin', [AlreadyLogin::class]);
 
+// Register Page
+Router::add('GET', '/register', UserController::class, 'register');
+Router::add('POST', '/register', UserController::class, 'postRegister');
 
 // Run Route
 Router::run();
