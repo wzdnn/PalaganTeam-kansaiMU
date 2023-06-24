dbc45e123f2a863d147fc0cd54a30c95a7deca7b
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-25 00:51:02 +0700

    Penggabungan dan perubahan tampilan UI (Merge frontend #1)

--- app/View/User/user-login.php
+++ app/View/User/user-login.php
@@ -3,102 +3,68 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link href="<?= ROOT ?>/dist/output.css" rel="stylesheet" />
     <title>Login</title>
+    <!-- LINK Start-->
+    <link rel="stylesheet" href="<?= ROOT?>/css/aos.css" />
+    <link rel="stylesheet" href="<?= ROOT?>/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?= ROOT?>/css/line-awesome.min.css" />
+    <link rel="stylesheet" href="<?= ROOT?>/css/loginstyle.css" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" /> -->
+    <!-- LINK END -->
   </head>
   <body>
-    <section class="bg-mublue/80 dark:bg-gray-900">
-      <div
-        class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
-      >
-        <div
-          class="w-full bg-white rounded-lg shadow-lg dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
-        >
-          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1
-              class="text-xl font-bold leading-tight tracking-tight text-center text-gray-900 md:text-2xl dark:text-white"
-            >
-              Selamat Datang
-            </h1>
-            <?php if(isset($response['error'])){?>
-              <p><?= $response['error']?></p>
-            <?php }?>
-            <form class="space-y-4 md:space-y-6" method="post">
-              <div>
-                <label
-                  for="email"
-                  class="block mb-2 text-sm font-bold text-gray-900 dark:text-white"
-                  >Email</label
-                >
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Silahkan Masukkan Email"
-                  required=""
-                />
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+      <div class="row-md border rounded-5 p-3 bg-white shadow box-area">
+        <div class="col-md">
+          <div class="row align-items-center">
+            <div class="header-text mt-4">
+              <h3>Hi, Welcome Back</h3>
+            </div>
+            <div class="header-text mb-1">
+              <p class="text-color-base text-center">It's Good To See You Again</p>
+              <?php if(isset($response['error'])){?>
+              <p class="text-center"><?= $response['error'] ?></p>
+              <?php }?>
+            </div>
+            <form method="post" name="form" onsubmit="return validated()" class="py-5">
+              <div class="form-floating mb-3">
+                <input autocomplete="off" type="email" name="email" class="form-control" id="floatingInput" placeholder=" Email" />
+                <label for="floatingInput" class="text-color-base"> Email address </label>
+                <div class="email_error" id="email_error">Silahkan isi email terlebih dahulu</div>
               </div>
-              <div>
-                <label
-                  for="password"
-                  class="block mb-2 text-sm font-bold text-gray-900 dark:text-white"
-                  >Password</label
-                >
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="Silahkan Masukkan Password"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  required=""
-                />
+              <div class="form-floating">
+                <input type="password" name="password" class="form-control" id="floatingPassword" placeholder=" Password" />
+                <label for="floatingPassword" class="text-color-base"> Password </label>
+                <div class="password_error" id="password_error">Silahkan isi password terlebih dahulu</div>
               </div>
-              <div class="flex items-center justify-between">
-                <div class="flex items-start">
-                  <div class="flex items-center h-5">
-                    <input
-                      id="remember"
-                      aria-describedby="remember"
-                      type="checkbox"
-                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                    />
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label
-                      for="remember"
-                      class="text-gray-500 dark:text-gray-300"
-                      >Remember me</label
-                    >
-                  </div>
+              <div class="input-group mt-3 d-flex gap-2 justify-content-start">
+                <input type="checkbox" name="remember" id="remember">
+                <label for="remember">remember me</label>
+              </div>
+              <div class="input-group mt-3 mb-3 d-flex justify-content-end">
+                <div class="forgot">
+                  <small><a href="#">Forgot Password?</a></small>
                 </div>
-                <a
-                  href="#"
-                  class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >Lupa Password</a
-                >
               </div>
-              <button
-                type="submit"
-                class="w-full text-white bg-mublue hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-              >
-                Login
-              </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                Belum punya akun?
-                <a
-                  href="#"
-                  class="font-medium text-mublue hover:underline dark:text-primary"
-                  >Daftar Baru</a
-                >
-              </p>
+              <div class="input-group mb-3 justify-content-center my-5">
+                <button type="submit" class="btn btn-lg btn-primary w-50 fs-6 shadow">Login</button>
+              </div>
             </form>
+
+            <div class="row">
+              <small>Not a member? <a href="/register">Register</a></small>
+            </div>
           </div>
         </div>
-        <a href="#home" class="font-medium text-primary hover:underline">
-          Kembali ke Home
-        </a>
       </div>
-    </section>
+    </div>
+
+    <!-- JS Script Start -->
+    <script src="<?= ROOT?>/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= ROOT?>/js/aos.js"></script>
+    <script src="<?= ROOT?>/js/main.js"></script>
+    <script src="<?= ROOT?>/js/loginValidation.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> -->
+    <!-- JS Script End -->
   </body>
 </html>
---
+++ app/View/User/user-register.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Register</title>
+    <!-- LINK Start-->
+    <link rel="stylesheet" href="<?= ROOT?>/css/aos.css" />
+    <link rel="stylesheet" href="<?= ROOT?>/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="<?= ROOT?>/css/line-awesome.min.css" />
+    <link rel="stylesheet" href="<?= ROOT?>/css/registerstyle.css" />
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous" /> -->
+    <!-- LINK END -->
+  </head>
+  <body>
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+      <div class="row-md border rounded-5 p-3 bg-white shadow box-area">
+        <div class="col-md">
+          <div class="row align-items-center">
+            <div class="header-text py-4">
+              <h1>Create An Account</h1>
+              <?php if(isset($response['error'])){?>
+              <p class="text-center"><?= $response['error'] ?></p>
+              <?php }?>
+            </div>
+            <form action="login.html" method="post" name="form" id="form" onsubmit="return validated()">
+              <div class="form-floating mb-3">
+                <input type="text" id="nama" name="fullname" class="form-control" placeholder="Nama" />
+                <label for="floatingInput" class="text-color-base"> Full Name </label>
+                <i class="fas fa-check-circle"></i>
+                <i class="fas fa-exclamation-circle"></i>
+                <small>Please Fill In</small>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="email" id="email" name="email" class="form-control" placeholder=" Email" />
+                <label for="floatingInput" class="text-color-base"> Email </label>
+                <i class="fas fa-check-circle"></i>
+                <i class="fas fa-exclamation-circle"></i>
+                <small>Please Fill In</small>
+              </div>
+              <div class="form-floating mb-3">
+                <input type="password" id="password" name="password" class="form-control" placeholder=" Password" />
+                <label for="floatingPassword" class="text-color-base"> Password </label>
+                <i class="fas fa-check-circle"></i>
+                <i class="fas fa-exclamation-circle"></i>
+                <small>Please Fill In</small>
+              </div>
+              <div class="form-floating mb-5">
+                <input type="password" id="password2" name="repassword" class="form-control" placeholder=" Password" />
+                <label for="floatingPassword" class="text-color-base"> Re-Password </label>
+                <i class="fas fa-check-circle"></i>
+                <i class="fas fa-exclamation-circle"></i>
+                <small>Please Fill In</small>
+              </div>
+              <div class="input-group mb-5 mt-5">
+                <button type="submit" class="btn btn-lg btn-primary w-100 fs-6">Register</button>
+              </div>
+            </form>
+            <div class="row">
+              <a class="text-color-base" href="/login">Back To Login</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- JS Script Start -->
+    <script src="<?= ROOT?>/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= ROOT?>/js/aos.js"></script>
+    <script src="<?= ROOT?>/js/main.js"></script>
+    <script src="<?= ROOT?>/js/registerValidation.js"></script>
+    <script src="https://kit.fontawesome.com/75d934ac69.js" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script> -->
+    <!-- JS Script End -->
+  </body>
+</html>
