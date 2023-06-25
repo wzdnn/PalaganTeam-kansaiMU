9a7de92a2948dc6720f4da5ffede91c277187c5b
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-25 21:01:23 +0700

    Penambahan verified account function

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -3,12 +3,42 @@
 namespace PalaganTeam\MuhKansai\Controller;
 
 use PalaganTeam\MuhKansai\App\View;
+use PalaganTeam\MuhKansai\Config\Database;
+use PalaganTeam\MuhKansai\Repository\UserRepository;
+use PalaganTeam\MuhKansai\Service\AccountService;
 
 class HomeController{
+    private AccountService $accountService;
+    public function __construct() {
+        $this->accountService = new AccountService();
+    }
     /**
-     * Web Home/Profile Page
+     * GET Web Home/Profile Page
      */
     public function index(){
         View::render('Home/index');
+    }
+
+    /**
+     * GET Account Verified
+     */
+    public function verified(string $vkey){
+        try{
+            if(isset($_GET['email']) && isset($vkey)){
+                if($this->accountService->verifiedAccount($vkey, $_GET['email'])){
+                    View::render('Home/account-verified', [
+                        'succses' => true,
+                        'massage' => 'E-Mail Has Been Verified',
+                        'email' => $_GET['email']
+                    ]);
+                }
+            }else{
+                View::redirect('/');
+            }
+        }catch(\Exception $ex){
+            View::render('Home/account-verified', [
+                'massage' => $ex->getMessage()
+            ]);
+        }
     }
 }
