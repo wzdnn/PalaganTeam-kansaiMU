cb4919012b75cd1e5cde30df74ac1ae09e29668e
Daffa Alif Murtaja <daffaalif1239@gmail.com>
2023-06-25 21:02:23 +0700

    Penambahan route baru untuk verified account

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use PalaganTeam\MuhKansai\Middleware\AlreadyLogin;
 use PalaganTeam\MuhKansai\Middleware\MustLoginMiddleware;
 
 require_once(__DIR__ . "./../vendor/autoload.php");
+// error_reporting(E_ERROR | E_PARSE);
 
 // Home Page Index(default)
 Router::add('GET', '/', HomeController::class, 'index', [MustLoginMiddleware::class]);
@@ -17,6 +18,10 @@ Router::add('POST', '/login', UserController::class, 'postLogin', [AlreadyLogin:
 // Register Page
 Router::add('GET', '/register', UserController::class, 'register');
 Router::add('POST', '/register', UserController::class, 'postRegister');
+
+
+// Account verified
+Router::add('GET', '/account/verified/([a-zA-Z0-9]*)', HomeController::class, 'verified');
 
 // Run Route
 Router::run();
