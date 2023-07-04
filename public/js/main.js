b31ff051f559a2fe75608713f72a7922a7134301
Zidan Zulkhairyan Mandalim <zidanzulkhairyan@gmail.com>
2023-07-04 10:50:06 +0700

    penambahan input news page dan activity page

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
 // dashboard dropdown sub button
 
-const subbtn = document.querySelectorAll('a');
+const menuBtn = document.getElementsByClassName('menu-btn');
+const subBtn = document.getElementsByClassName('sub-btn');
+const closeBtn = document.getElementsByClassName('close-btn');
+const rotate = document.getElementsByClassName('rotate');
+const dropdown = document.getElementsByClassName('dropdown');
+const active = document.getElementsByClassName('active');
+
+function sideBar() {
+  $(subBtn).click(function () {
+    $(this).next('.sub-menu').slideToggle();
+    $(this).find('.dropdown').toggleClass('rotate');
+  });
+  $('.menu-btn').click(function () {
+    $('.side-bar').addClass('active');
+    $('.menu-btn').css('visibility', 'hidden');
+  });
+
+  $('.close-btn').click(function () {
+    $('.side-bar').removeClass('active');
+    $('.menu-btn').css('visibility', 'visible');
+  });
+}
